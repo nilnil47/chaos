0680b0b054e9e92872ae0b77902620a1fb0f26d4
nilnil47 <nilnil47@gmail.com>
2023-05-04 12:29:28 +0300

    interval inside peaks works

--- peaks.m
+++ peaks.m
@@ -12,12 +12,13 @@ for i= 1:length(data) - 1
     if (data(i+1) - data(i) > threshold)
         locs_up = [locs_up, i+1];
         pks_up = [pks_up, data(i+1)];
-        % if isempty(locs_down)
-        %     last_loc = 0;
-        % else
-        %     last_loc = locs_down(end);
-        % 
-        % intervals = [intervals, i+1 - last_loc];
+        if isempty(locs_down)
+            last_loc = 0;
+        else
+            last_loc = locs_down(end);
+        end
+
+        intervals = [intervals, i+1 - last_loc];
     end
     if (data(i+1) - data(i) < -threshold)
         locs_down = [locs_down, i];
